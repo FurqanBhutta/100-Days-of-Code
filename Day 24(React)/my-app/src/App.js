b985c7f843f7416446cacad37b7ac6ed59203c3a
furqan bhutta <bhutta.furqan9@gmail.com>
2026-07-21 09:17:26 +0500

    Day 24 completed. Starting react.

---
+++ Day 24(React)/my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './Components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <h1>Hello, I am Muhammad Furqan!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day 24(React)/my-app/src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css';
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav
